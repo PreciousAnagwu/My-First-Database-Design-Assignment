e594d9f980ed8b030fb220321be8fb02dc78cd72
PreciousAnagwu <panagwu@gmail.com>
2025-08-05 10:02:38 +0100

    readability

--- answers.sql
+++ answers.sql
@@ -1,7 +1,7 @@
 --My First Assignment on Database
 
 1.--Create a Database called SalesDB
-    CREATE DATABASE salesDB;--Solution to number 1
+    CREATE DATABASE salesDB;  --Solution to number 1
 
 
 2.  --Drop/delete a database called demo
