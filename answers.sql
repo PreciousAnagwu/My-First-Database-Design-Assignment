7ad6459b5059ca0267ff081a7724cfc7b270cd13
PreciousAnagwu <panagwu@gmail.com>
2025-08-05 09:43:05 +0100

    databse

---
+++ answers.sql
@@ -0,0 +1,9 @@
+--My First Assignment on Database
+
+1.--Create a Database called SalesDB
+    CREATE DATABASE salesDB;--Solution to number 1
+
+
+2.  --Drop/delete a database called demo
+    DROP DATABASE demo;  --Solution to number 2
+
